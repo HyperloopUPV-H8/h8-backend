c61db24f5e9849e3f6fab9e388de5ec281b4b829
lipezaballa <lipezaballa@gmail.com>
2022-10-21 16:33:35 +0200

    DEV-2: change name to excelRetriever.go, several methods in readExcel.go
    getSheet, getTables, getTable, getInitOfTables, getMaxRowLength, getNumberRows, geRectangularTable

--- src/DataTransfer/infra/excel/readExcel.go
+++ src/DataTransfer/infra/excel/readExcel.go
@@ -43,64 +43,125 @@ func GetSheets(f *excelize.File) map[string]Sheet {
 	newMap := make(map[string]Sheet)
 	namesMap := f.GetSheetMap()
 	for _, name := range namesMap {
-		sheetContent, _ := f.GetRows(name)
-		tables := getTables(sheetContent)
-		sheet := Sheet{
-			Name:   name,
-			Tables: tables,
-		}
+		sheet := getSheet(name, f)
 		newMap[name] = sheet
 	}
 	return newMap
 }
 
-//QUIZÁ CREAR MÉTODO QUE HAGA COPIA RECTANGULAR DE LA SHEET COMPLETA? EN VEZ DE TENER QUE COMPROBARLO PARA CADA TABLA
+func getSheet(name string, f *excelize.File) Sheet {
+	sheetContent, _ := f.GetRows(name)
+	tables := getTables(sheetContent)
+	sheet := Sheet{
+		Name:   name,
+		Tables: tables,
+	}
+	return sheet
+}
 
-// POR IMPLEMENTAR
+// QUIZÁ CREAR MÉTODO QUE HAGA COPIA RECTANGULAR DE LA SHEET COMPLETA? EN VEZ DE TENER QUE COMPROBARLO PARA CADA TABLA
+// en segunda fila están todas las cabeceras
+// func getMaxRowLengthOfSheet(sheetContent [][]string) int {
+// 	maxRowLength := -1
+// 	for i := 0; i < len(sheetContent); i++ {
+// 		if maxRowLength < len(sheetContent[i]) {
+// 			maxRowLength = len(sheetContent[i])
+// 		}
+// 	}
+// 	return maxRowLength
+// }
+
+func getMaxRowLength(propertiesRow []string, initColumn int) int {
+	maxRowLength := 0
+	emptyCell := false
+	i := initColumn
+	for {
+		emptyCell = propertiesRow[i] == ""
+		if emptyCell {
+			break
+		}
+		maxRowLength++
+		i++
+	}
+
+	return maxRowLength
+}
+
+func getNumberRows(sheetContent [][]string, initDates [2]int) int {
+	numberRows := 0
+	emptyCell := false
+	i := initDates[0]
+	j := initDates[1]
+	for {
+		emptyCell = sheetContent[i][j] == ""
+		if emptyCell {
+			break
+		}
+		numberRows++
+		j++
+	}
+
+	return numberRows
+}
+
 func getTables(sheetContent [][]string) map[string]Table {
 	tables := make(map[string]Table)
-	var axis [2]int = [2]int{0, 0}
-	for axis[0] < len(sheetContent) {
-		axis = getInitOfTable(sheetContent, axis)
-		tableName := sheetContent[axis[0]][axis[1]]
-
-		var tableContent [][]string //FALTA DE IMPLEMENTAR, hacer copia de la sheet que sea rectangular
-		rows := getRowsOfTable(tableContent)
-
-		table := Table{
-			Name: tableName,
-			Rows: rows,
-		}
-		tables[tableName] = table
-
-		axis[1]++
-
+	initCells := getInitOfTables(sheetContent)
+	for i := 0; i < len(initCells); i++ {
+		table := getTable(sheetContent, initCells[i])
+		tables[table.Name] = table
 	}
 
 	return tables
 }
 
-// Otra opción, podía crear nuevo [][]string con lo que queda por recorrer
-func getInitOfTable(sheetContent [][]string, axis [2]int) [2]int {
+func getInitOfTables(sheetContent [][]string) [][2]int {
 	initString := "[TABLE]"
-	finded := false
+	found := false
+	var initCells [][2]int
 
-	for i := axis[0]; i < len(sheetContent); i++ {
-		for j := axis[1]; j < len(sheetContent[i]); j++ {
+	for i := 0; i < len(sheetContent); i++ {
+		for j := 0; j < len(sheetContent[i]); j++ {
 			cellValue := sheetContent[i][j]
-			finded = strings.Contains(cellValue, initString)
-			if finded {
-				axis[0] = i
-				axis[1] = j
-				break
+			found = strings.Contains(cellValue, initString)
+			if found {
+				var initCell [2]int = [2]int{i, j}
+				initCells = append(initCells, initCell)
+				found = false
 			}
 		}
-		if finded {
-			break
-		}
+
 	}
 
-	return axis
+	return initCells
+}
+
+func getTable(sheetContent [][]string, axis [2]int) Table {
+
+	tableName := sheetContent[axis[0]][axis[1]]
+	propertiesRow := sheetContent[axis[0]+1]
+	rowLengthTable := getMaxRowLength(propertiesRow, axis[1])
+
+	var initDates [2]int = [2]int{axis[0] + 2, axis[1]}
+	numberRows := getNumberRows(sheetContent, initDates)
+	rectangularTable := getRectangularTable(sheetContent, rowLengthTable, numberRows, initDates)
+
+	var tableContent [][]string //FALTA DE IMPLEMENTAR, hacer copia de la sheet que sea rectangular
+	rows := getRowsOfTable(tableContent)
+
+	table := Table{
+		Name: tableName,
+		Rows: rows,
+	}
+
+	return table
+}
+
+// POR IMPLEMENTAR
+func getRectangularTable(sheetContent [][]string, maxRowLength int, numberRows int, initDatesOfTable [2]int) [][]string {
+	rectangularTable := make([][]string, maxRowLength)
+
+	return rectangularTable
 }
 
 // Recibes la hoja cortada
