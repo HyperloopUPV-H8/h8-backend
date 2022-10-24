191fad582036e8d0071d4f1be63715b61ec4d9c2
Sergio <smorsua@teleco.upv.es>
2022-10-24 19:52:40 +0200

    DEV-3 Removed comments

--- src/DataTransfer/podDataCreator/domain/measurement/value/bool.go
+++ src/DataTransfer/podDataCreator/domain/measurement/value/bool.go
@@ -6,18 +6,6 @@ import (
 
 type Bool bool
 
-// func (b *Bool) current() Value {
-// 	return b
-// }
-
-// func (i *Bool) fromBuffer(b *bytes.Buffer) {
-// 	binary.Read(b, binary.BigEndian, i)
-// }
-
-// func (b *Bool) toString() string {
-// 	return fmt.Sprintf("%v", b)
-// }
-
 func (b *Bool) ToDisplayString() string {
 	return fmt.Sprintf("%v", b)
 }
--- src/DataTransfer/podDataCreator/domain/measurement/value/value.go
+++ src/DataTransfer/podDataCreator/domain/measurement/value/value.go
@@ -5,9 +5,6 @@ import (
 )
 
 type Value interface {
-	//current() Value
-	//fromBuffer(b *bytes.Buffer)
-	//toString() string
 	ToDisplayString() string
 }
 
@@ -38,36 +35,3 @@ func isNumber(valueType string) bool {
 func isEnum(va string) bool {
 	return true
 }
-
-// func New(valueType string) Value {
-// 	switch valueType {
-// 	case "uint8":
-// 		return new(UInt8)
-// 	case "uint16":
-// 		return new(UInt16)
-// 	case "uint32":
-// 		return new(UInt32)
-// 	case "uint64":
-// 		return new(UInt64)
-// 	case "int8":
-// 		return new(Int8)
-// 	case "int16":
-// 		return new(Int16)
-// 	case "int32":
-// 		return new(Int32)
-// 	case "int64":
-// 		return new(Int64)
-// 	case "float32":
-// 		return new(Float32)
-// 	case "float64":
-// 		return new(Float64)
-// 	case "bool":
-// 		return new(Bool)
-// 	default:
-// 		if isEnum(valueType) {
-// 			return newEnum(valueType)
-// 		} else {
-// 			panic("Invalid type")
-// 		}
-// 	}
-// }
