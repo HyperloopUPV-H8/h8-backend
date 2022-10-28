07bbe20ff1a3ce0513613494fcafce09a68e86bf
Sergio <smorsua@teleco.upv.es>
2022-10-28 13:16:33 +0200

    DEV-3 Added main for tests

--- src/cmd/MVP-1/main.go
+++ src/cmd/MVP-1/main.go
@@ -1,38 +1,47 @@
-package mvp1
+package main
 
-import "github.com/HyperloopUPV-H8/Backend-H8/Shared/excelRetriever"
+import (
+	"fmt"
 
-var Sheets = map[string]excelRetriever.Sheet{
-	"BMS": {
-		Name: "BMS",
-		Tables: map[string]excelRetriever.Table{
-			"Packet Description": {
-				Name: "Packet Description",
-				Rows: [][]string{
-					{"1[0,5]", "Voltages", "200", "Input", "TCP"},
-					{"2", "Speeds", "300", "Input", "UDP"},
-					{"3", "Currents", "400", "Input", "TCP"},
-					{"4", "Airgaps", "500", "Output", "UDP"},
-					{"5", "Positions", "600", "Input", "UDP"},
-					{"6", "Batteries", "700", "Input", "TCP"},
+	packetadapter "github.com/HyperloopUPV-H8/Backend-H8/Shared/PacketAdapter"
+	"github.com/HyperloopUPV-H8/Backend-H8/Shared/excelAdapter"
+	"github.com/HyperloopUPV-H8/Backend-H8/Shared/excelAdapter/dto"
+	"github.com/HyperloopUPV-H8/Backend-H8/Shared/excelRetriever"
+)
+
+var structure = excelRetriever.Structure{
+	Sheets: map[string]excelRetriever.Sheet{
+		"BMS": {
+			Name: "BMS",
+			Tables: map[string]excelRetriever.Table{
+				"Packet Description": {
+					Name: "Packet Description",
+					Rows: [][]string{
+						{"1[0,5]", "Voltages", "200", "Input", "TCP"},
+						{"2", "Speeds", "300", "Input", "UDP"},
+						{"3", "Currents", "400", "Input", "TCP"},
+						{"4", "Airgaps", "500", "Output", "UDP"},
+						{"5", "Positions", "600", "Input", "UDP"},
+						{"6", "Batteries", "700", "Input", "TCP"},
+					},
 				},
-			},
-			"Value Description": {
-				Name: "Value Description",
-				Rows: [][]string{
-					{"Voltage1", "uint8", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
-					{"Speed1", "uint16", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
-					{"Current1", "uint32", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
-					{"Airgap1", "uint64", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
-					{"Position1", "int8", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
-					{"Battery1", "int16", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
+				"Value Description": {
+					Name: "Value Description",
+					Rows: [][]string{
+						{"Voltage1", "uint8", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
+						{"Speed1", "uint16", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
+						{"Current1", "uint32", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
+						{"Airgap1", "uint64", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
+						{"Position1", "int8", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
+						{"Battery1", "int16", "cdeg#/100#", "deg##", "[0,10]", "[-10,20]"},
+					},
 				},
-			},
-			"Packet Structure": {
-				Name: "Packet Structure",
-				Rows: [][]string{
-					{"Voltages", "Speeds", "Currents", "Airgaps", "Positions", "Batteries"},
-					{"Voltage1", "Speed1", "Current1", "Airgap1", "Position1", "Battery1"},
+				"Packet Structure": {
+					Name: "Packet Structure",
+					Rows: [][]string{
+						{"Voltages", "Speeds", "Currents", "Airgaps", "Positions", "Batteries"},
+						{"Voltage1", "Speed1", "Current1", "Airgap1", "Position1", "Battery1"},
+					},
 				},
 			},
 		},
@@ -40,5 +49,18 @@ var Sheets = map[string]excelRetriever.Sheet{
 }
 
 func main() {
+	ips := []string{"127.0.0.1"}
 
+	boardDTOs := excelAdapter.GetBoardDTOs(structure)
+	packetDTOs := make([]dto.PacketDTO, 0)
+	for _, boardDTO := range boardDTOs {
+		packetDTOs = append(packetDTOs, boardDTO.GetPacketDTOs()...)
+	}
+
+	packetAdapter := packetadapter.New(packetDTOs, ips)
+	fmt.Println("Starting loop")
+	for {
+		updates := packetAdapter.GetPacketUpdates()
+		fmt.Println(updates)
+	}
 }
