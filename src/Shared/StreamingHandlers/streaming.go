f1807f57ccc2e450193860e3be120e9180906f9e
Sergio <smorsua@teleco.upv.es>
2022-11-02 16:26:25 +0100

    DEV-13 Directory reestructure

--- src/Shared/StreamingHandlers/streaming.go
+++ src/Shared/StreamingHandlers/streaming.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// TODO: Break into multiple functions
 func DataSocketHandler(ws websocket.Conn, packetChannel <-chan domain.Packet) {
 	go func() {
 		for {
