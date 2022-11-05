b7fd105144ee4e86fc102161e5966c5c7fc1537c
jmaralo <martinezalonsojn@gmail.com>
2022-11-05 17:33:54 +0100

    DEV-26 add message transfer and message streaming routine

---
+++ src/MessageTransfer/message_transfer.go
@@ -0,0 +1,19 @@
+package ordertransfer
+
+import packetParser "github.com/HyperloopUPV-H8/Backend-H8/Shared/packet_adapter/packet_parser/domain"
+
+type MessageTransfer struct {
+	MessageChannel chan packetParser.PacketUpdate
+}
+
+func New() MessageTransfer {
+	return MessageTransfer{
+		MessageChannel: make(chan packetParser.PacketUpdate),
+	}
+}
+
+func (messageTransfer MessageTransfer) Invoke(getMessage func() packetParser.PacketUpdate) {
+	for {
+		messageTransfer.MessageChannel <- getMessage()
+	}
+}
