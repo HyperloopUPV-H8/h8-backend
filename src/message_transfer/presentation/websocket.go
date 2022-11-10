187b5a5a91dbbcb81a57978b8e05b7e6ba865470
Juan Martinez <martinezalonsojn@gmail.com>
2022-11-10 16:11:49 +0100

    DEV-42 add MessageTransfer

---
+++ src/message_transfer/presentation/websocket.go
@@ -0,0 +1,15 @@
+package presentation
+
+import (
+	"github.com/HyperloopUPV-H8/Backend-H8/Shared/server/infra/interfaces"
+	"github.com/HyperloopUPV-H8/Backend-H8/message_transfer/domain"
+)
+
+func MessageRoutine(socket interfaces.WebSocket, messages <-chan domain.Message) {
+	go func() {
+		var err error
+		for err == nil {
+			err = socket.WriteJSON(<-messages)
+		}
+	}()
+}
