e46ea5019ee65509470da5370b195ec3de954616
jmaralo <martinezalonsojn@gmail.com>
2022-11-16 18:00:24 +0100

    DEV-52 fix server packet struct

--- src/Shared/Server/domain/packet.go
+++ src/Shared/Server/domain/packet.go
@@ -3,9 +3,6 @@ package domain
 type Packet struct {
 	Id           uint16                 `json:"id"`
 	Name         string                 `json:"name"`
-	Frequency    uint                   `json:"frequency"`
-	Direction    string                 `json:"direction"`
-	Protocol     string                 `json:"direction"`
 	HexValue     string                 `json:"hexValue"`
 	Measurements map[string]Measurement `json:"measurements"`
 	Count        uint                   `json:"count"`
