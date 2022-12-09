728ee65612ad38765c78ec72966df8eff235b17d
jmaralo <martinezalonsojn@gmail.com>
2022-12-09 01:18:17 +0100

    DEV-70 add dataTransfer PacketFactory

---
+++ src/data_transfer/packet_factory.go
@@ -0,0 +1,48 @@
+package data_transfer
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/HyperloopUPV-H8/Backend-H8/data_transfer/models"
+)
+
+type PacketFactory struct {
+	count     map[uint16]uint64
+	timestamp map[uint16]uint64
+}
+
+func NewFactory() PacketFactory {
+	return PacketFactory{
+		count:     make(map[uint16]uint64),
+		timestamp: make(map[uint16]uint64),
+	}
+}
+
+func (factory PacketFactory) NewPacket(id uint16, raw []byte, values map[string]any) models.PacketUpdate {
+	count, cycleTime := factory.getNext(id)
+	return models.PacketUpdate{
+		ID:        id,
+		HexValue:  fmt.Sprintf("%x", raw),
+		Values:    formatValues(values),
+		Count:     count,
+		CycleTime: cycleTime,
+	}
+}
+
+func formatValues(values map[string]any) map[string]string {
+	mapped := make(map[string]string, len(values))
+	for name, val := range values {
+		mapped[name] = fmt.Sprintf("%v", val)
+	}
+	return mapped
+}
+
+func (factory PacketFactory) getNext(id uint16) (count uint64, cycleTime uint64) {
+	timestamp := uint64(time.Now().UnixNano())
+	cycleTime = timestamp - factory.timestamp[id]
+	factory.timestamp[id] = timestamp
+	factory.count[id] += 1
+	count = factory.count[id]
+	return
+}
