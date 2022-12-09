46caa317fdd76a92cd29706f3e23cd9745bfdbcd
jmaralo <martinezalonsojn@gmail.com>
2022-12-09 01:10:30 +0100

    DEV-70 add packetParser

---
+++ src/packet_parser/packet_parser.go
@@ -0,0 +1,104 @@
+package packet_parser
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"strconv"
+	"strings"
+
+	excelAdapterModels "github.com/HyperloopUPV-H8/Backend-H8/excel_adapter/models"
+	"github.com/HyperloopUPV-H8/Backend-H8/packet_parser/internals"
+	"github.com/HyperloopUPV-H8/Backend-H8/packet_parser/models"
+)
+
+type PacketParser struct {
+	descriptors map[uint16]models.PacketDescriptor
+	enums       map[string]models.Enum
+}
+
+func (parser *PacketParser) AddPacket(board string, ip string, desc excelAdapterModels.Description, values []excelAdapterModels.Value) {
+	if parser.descriptors == nil {
+		parser.descriptors = make(map[uint16]models.PacketDescriptor)
+	}
+
+	id, err := strconv.ParseUint(desc.ID, 10, 16)
+	if err != nil {
+		log.Fatalf("packet parser: AddPacket: %s\n", err)
+	}
+
+	valueDescriptors := make([]models.ValueDescriptor, len(values))
+	for i, value := range values {
+		kind := value.Type
+		if strings.HasPrefix(kind, "ENUM") {
+			kind = "enum"
+			if parser.enums == nil {
+				parser.enums = make(map[string]models.Enum)
+			}
+			parser.enums[value.Name] = models.GetEnum(value.Type)
+		}
+
+		valueDescriptors[i] = models.ValueDescriptor{
+			Name: value.Name,
+			Type: kind,
+		}
+	}
+
+	parser.descriptors[uint16(id)] = valueDescriptors
+}
+
+func NewPacketParser(descriptors map[uint16]models.PacketDescriptor, enums map[string]models.Enum) PacketParser {
+	return PacketParser{
+		descriptors: descriptors,
+		enums:       enums,
+	}
+}
+
+func (parser PacketParser) Decode(raw []byte) (id uint16, values models.PacketValues) {
+	reader := bytes.NewReader(raw)
+	id = internals.DecodeID(reader)
+
+	values = make(models.PacketValues, len(parser.descriptors[id]))
+	for _, value := range parser.descriptors[id] {
+		values[value.Name] = parser.decodeValue(value, reader)
+	}
+
+	return id, values
+}
+
+func (parser PacketParser) decodeValue(value models.ValueDescriptor, reader io.Reader) any {
+	switch value.Type {
+	case "enum":
+		return internals.DecodeEnum(reader, parser.enums[value.Name])
+	case "bool":
+		return internals.DecodeBool(reader)
+	case "string":
+		return internals.DecodeString(reader)
+	default:
+		return internals.DecodeNumber(reader, value.Type)
+	}
+}
+
+func (parser PacketParser) Encode(id uint16, values models.PacketValues) []byte {
+	writer := bytes.NewBuffer([]byte{})
+	internals.EncodeID(writer, id)
+
+	for _, value := range parser.descriptors[id] {
+		parser.encodeValue(value, values[value.Name], writer)
+	}
+
+	return writer.Bytes()
+}
+
+func (parser PacketParser) encodeValue(desc models.ValueDescriptor, value any, writer io.Writer) {
+	switch desc.Type {
+	case "enum":
+		internals.EncodeEnum(writer, parser.enums[desc.Name], value.(string))
+	case "bool":
+		internals.EncodeBool(writer, value.(bool))
+	case "string":
+		internals.EncodeString(writer, value.(string))
+	default:
+		internals.EncodeNumber(writer, desc.Type, value.(float64))
+	}
+}
