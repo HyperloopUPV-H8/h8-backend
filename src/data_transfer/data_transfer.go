a14c73272db98fad6d3c23ba465b52719e24da7f
jmaralo <martinezalonsojn@gmail.com>
2022-12-10 21:08:09 +0100

    DEV-70 sync all dataTransfer connections

--- src/data_transfer/data_transfer.go
+++ src/data_transfer/data_transfer.go
@@ -1,46 +1,64 @@
 package data_transfer
 
 import (
+	"encoding/json"
+	"log"
 	"sync"
 	"time"
 
 	"github.com/HyperloopUPV-H8/Backend-H8/data_transfer/models"
 	"github.com/gorilla/websocket"
+	"github.com/kjk/betterguid"
 )
 
 type DataTransfer struct {
 	bufMx     sync.Mutex
 	packetBuf map[uint16]models.PacketUpdate
-	rate      time.Duration
+	ticker    *time.Ticker
+	sockets   map[string]*websocket.Conn
 }
 
 func New(rate time.Duration) *DataTransfer {
-	return &DataTransfer{
+	dataTransfer := &DataTransfer{
 		bufMx:     sync.Mutex{},
 		packetBuf: make(map[uint16]models.PacketUpdate),
-		rate:      rate,
+		ticker:    time.NewTicker(rate),
+		sockets:   make(map[string]*websocket.Conn),
+	}
+
+	go dataTransfer.run()
+
+	return dataTransfer
+}
+
+func (dataTransfer *DataTransfer) run() {
+	for {
+		<-dataTransfer.ticker.C
+		data := dataTransfer.getJSON()
+		for id, socket := range dataTransfer.sockets {
+			if err := socket.WriteMessage(websocket.TextMessage, data); err != nil {
+				delete(dataTransfer.sockets, id)
+			}
+		}
 	}
 }
 
-func (dataTransfer *DataTransfer) HandleConn(socket *websocket.Conn) {
-	go func(socket *websocket.Conn) {
-		defer socket.Close()
-		ticker := time.NewTicker(dataTransfer.rate)
+func (dataTransfer *DataTransfer) getJSON() []byte {
+	dataTransfer.bufMx.Lock()
+	defer dataTransfer.bufMx.Unlock()
+	data, err := json.Marshal(dataTransfer.packetBuf)
+	if err != nil {
+		log.Fatalf("data transfer: getJSON: %s\n", err)
+	}
+	return data
+}
 
-		for {
-			<-ticker.C
-			dataTransfer.bufMx.Lock()
-			if err := socket.WriteJSON(dataTransfer.packetBuf); err != nil {
-				dataTransfer.bufMx.Unlock()
-				break
-			}
-			dataTransfer.bufMx.Unlock()
-		}
-	}(socket)
+func (dataTransfer *DataTransfer) HandleConn(socket *websocket.Conn) {
+	dataTransfer.sockets[betterguid.New()] = socket
 }
 
 func (dataTransfer *DataTransfer) Update(update models.PacketUpdate) {
 	dataTransfer.bufMx.Lock()
+	defer dataTransfer.bufMx.Unlock()
 	dataTransfer.packetBuf[update.ID] = update
-	dataTransfer.bufMx.Unlock()
 }
