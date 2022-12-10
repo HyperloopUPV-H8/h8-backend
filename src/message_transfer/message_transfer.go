a8f911b44b630e13495fe457f3adb6da04a12ecb
jmaralo <martinezalonsojn@gmail.com>
2022-12-10 18:01:46 +0100

    DEV-70 keep a list of WS instead of channels

--- src/message_transfer/message_transfer.go
+++ src/message_transfer/message_transfer.go
@@ -7,41 +7,36 @@ import (
 )
 
 type MessageTransfer struct {
-	routines []chan<- dataTransferModels.PacketUpdate
+	sockets map[string]*websocket.Conn
 }
 
 func (messageTransfer *MessageTransfer) HandleConn(socket *websocket.Conn) {
-	updates := make(chan dataTransferModels.PacketUpdate)
-	messageTransfer.routines = append(messageTransfer.routines, updates)
-
-	go func(socket *websocket.Conn, updates <-chan dataTransferModels.PacketUpdate) {
-		defer socket.Close()
-		for {
-			update := <-updates
-			var message models.Message
-			if msg, ok := update.Values["warning"]; ok {
-				message = models.Message{
-					ID:          update.ID,
-					Description: msg,
-					Type:        "warning",
-				}
-			} else if msg, ok = update.Values["fault"]; ok {
-				message = models.Message{
-					ID:          update.ID,
-					Description: msg,
-					Type:        "warning",
-				}
-			}
-
-			if err := socket.WriteJSON(message); err != nil {
-				return
-			}
-		}
-	}(socket, updates)
+	messageTransfer.sockets[socket.RemoteAddr().String()] = socket
 }
 
 func (messageTransfer *MessageTransfer) Broadcast(update dataTransferModels.PacketUpdate) {
-	for _, routine := range messageTransfer.routines {
-		routine <- update
+	var message models.Message
+	if msg, ok := update.Values["warning"]; ok {
+		message = models.Message{
+			ID:          update.ID,
+			Description: msg,
+			Type:        "warning",
+		}
+	} else if msg, ok = update.Values["fault"]; ok {
+		message = models.Message{
+			ID:          update.ID,
+			Description: msg,
+			Type:        "warning",
+		}
+	}
+
+	closed := make([]string, 0, len(messageTransfer.sockets))
+	for name, socket := range messageTransfer.sockets {
+		if err := socket.WriteJSON(message); err != nil {
+			closed = append(closed, name)
+		}
+	}
+	for _, name := range closed {
+		delete(messageTransfer.sockets, name)
 	}
 }
