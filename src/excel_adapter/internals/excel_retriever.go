097b14413111c29ffb4f5f92194a28744ebcbef7
lipezaballa <lipezaballa@gmail.com>
2023-03-02 16:46:21 +0100

    DEV-115: Using offline excel

--- src/excel_adapter/internals/excel_retriever.go
+++ src/excel_adapter/internals/excel_retriever.go
@@ -13,43 +13,61 @@ import (
 
 const sheetsMimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
-func DownloadFile(id string, path string, name string) {
-	saveFile(getFile(getClient(), id, sheetsMimeType), path, name)
+func DownloadFile(id string, path string, name string) error {
+	client, errClient := getClient()
+	if errClient != nil {
+		return errClient
+	}
+
+	file, errFile := getFile(client, id, sheetsMimeType)
+	if errFile != nil {
+		return errFile
+	}
+
+	errSaving := saveFile(file, path, name)
+
+	return errSaving
 }
 
-func getClient() *drive.Service {
+func getClient() (*drive.Service, error) {
 	ctx := context.Background()
 
 	client, err := drive.NewService(ctx, option.WithCredentialsFile(os.Getenv("CREDENTIALS")))
 	if err != nil {
-		log.Fatalf("excel retriever: getClient: %s\n", err)
+		log.Printf("excel retriever: the client could not be obtained")
+		return nil, err
 	}
 
-	return client
+	return client, nil
 }
 
-func getFile(client *drive.Service, id string, mimeType string) []byte {
+func getFile(client *drive.Service, id string, mimeType string) ([]byte, error) {
 	resp, err := client.Files.Export(id, mimeType).Download()
 	if err != nil {
-		log.Fatalf("excel retriever: getFile: %s\n", err)
+		log.Printf("excel retriever: getFile: could not download the file")
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("excel retriever: getFile: %s\n", err)
+		log.Printf("excel retriever: getFile: could not read the http response body")
+		return nil, err
 	}
 
-	return data
+	return data, nil
 }
 
-func saveFile(content []byte, path string, name string) {
+func saveFile(content []byte, path string, name string) error {
 	err := os.Mkdir(path, os.ModeDir)
 	if !os.IsExist(err) {
-		log.Fatalf("excel retriever: saveFile: %s\n", err)
+		log.Printf("excel retriever: saveFile: could not create the directory")
+		return err
 	}
 	err = os.WriteFile(filepath.Join(path, name), content, 0644) // rw-r--r--
 	if err != nil {
-		log.Fatalf("excel retriever: saveFile: %s\n", err)
+		log.Printf("excel retriever: saveFile: could not write the file in the directory")
+		return err
 	}
+	return nil
 }
