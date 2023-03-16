bcf0e7b55d7b240b01915263a5f796f5717dede7
jmaralo <martinezalonsojn@gmail.com>
2023-03-16 11:19:21 +0100

    DEV-118 fix TransportController
    Before it made both as a server and as a client, now it is only a client

--- src/transport_controller/internals/pipe_handle.go
+++ src/transport_controller/internals/pipe_handle.go
@@ -1,50 +1,21 @@
 package internals
 
 import (
-	"log"
 	"net"
-	"strings"
 
 	"github.com/HyperloopUPV-H8/Backend-H8/transport_controller/models"
 )
 
 type PipeHandle struct {
-	listener *net.TCPListener
-	pipes    map[string]*Pipe
+	pipes map[string]*Pipe
 }
 
 func OpenPipes(laddr *net.TCPAddr, raddrs []*net.TCPAddr, config models.Config) *PipeHandle {
 	handle := &PipeHandle{
-		listener: bindListener(laddr),
-		pipes:    getPipes(laddr, raddrs, config),
+		pipes: getPipes(laddr, raddrs, config),
 	}
-
-	go handle.listen()
 
 	return handle
-}
-
-func bindListener(addr *net.TCPAddr) *net.TCPListener {
-	listener, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		log.Fatalf("PipeHandle: bindListener: %s\n", err)
-	}
-	return listener
-}
-
-func (handle *PipeHandle) listen() {
-	for handle.listener != nil {
-		conn, err := handle.listener.AcceptTCP()
-		if err != nil {
-			continue
-		}
-		pipe, ok := handle.pipes[withoutPort(conn.RemoteAddr())]
-		if !ok {
-			continue
-		}
-
-		pipe.open(conn)
-	}
 }
 
 func (handle *PipeHandle) Write(addr string, payload []byte) bool {
@@ -63,12 +34,7 @@ func getPipes(laddr *net.TCPAddr, raddrs []*net.TCPAddr, config models.Config) m
 	return pipes
 }
 
-func withoutPort(addr net.Addr) string {
-	return strings.Split(addr.String(), ":")[0]
-}
-
 func (handle *PipeHandle) Close() {
-	handle.listener.Close()
 	for _, pipe := range handle.pipes {
 		pipe.close()
 	}
