e79576865c2d20edf58befea697bc469b047fe76
jmaralo <martinezalonsojn@gmail.com>
2023-03-28 12:07:24 +0200

    DEV-120 new WebSocketBroker implementation
    The new implementation doesn't have any race conditions

---
+++ src/websocket_broker/models/message.go
@@ -0,0 +1,16 @@
+package models
+
+import "encoding/json"
+
+type Message struct {
+	Topic   string          `json:"topic"`
+	Payload json.RawMessage `json:"msg"`
+}
+
+func NewMessage(topic string, message any) (Message, error) {
+	messageRaw, err := json.Marshal(message)
+	return Message{
+		Topic:   topic,
+		Payload: messageRaw,
+	}, err
+}
