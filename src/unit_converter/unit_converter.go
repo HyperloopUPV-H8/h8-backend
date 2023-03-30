caade4c8416fed9d2abf1d945819b08d79d66f66
jmaralo <martinezalonsojn@gmail.com>
2023-03-30 16:48:40 +0200

    DEV-122 add tracing to unitConverter

--- src/unit_converter/unit_converter.go
+++ src/unit_converter/unit_converter.go
@@ -5,32 +5,45 @@ import (
 
 	excelAdapterModels "github.com/HyperloopUPV-H8/Backend-H8/excel_adapter/models"
 	"github.com/HyperloopUPV-H8/Backend-H8/unit_converter/models"
+	"github.com/rs/zerolog"
+	trace "github.com/rs/zerolog/log"
 )
 
 type UnitConverter struct {
 	Kind           string
 	operations     map[string]models.Operations
 	unitOperations map[string]string
+	trace          zerolog.Logger
 }
 
 func NewUnitConverter(kind string) *UnitConverter {
+	trace.Info().Str("kind", kind).Msg("new unit converter")
 	return &UnitConverter{
 		Kind:       kind,
 		operations: make(map[string]models.Operations),
+		trace:      trace.With().Str("component", "unitConverter").Str("kind", kind).Logger(),
 	}
 }
 
 func (converter *UnitConverter) AddGlobal(global excelAdapterModels.GlobalInfo) {
+	converter.trace.Debug().Msg("add global")
 	converter.unitOperations = global.UnitToOperations
 }
 
 func (converter *UnitConverter) AddPacket(boardName string, packet excelAdapterModels.Packet) {
+	converter.trace.Debug().Str("id", packet.Description.ID).Str("name", packet.Description.Name).Str("board", boardName).Msg("add packet")
 	for _, val := range packet.Values {
+		var ops string
 		if converter.Kind == "pod" && val.PodUnits != "" {
-			converter.operations[val.ID] = models.NewOperations(getCustomOrGlobalOperations(val.PodUnits, converter.unitOperations))
+			ops = getCustomOrGlobalOperations(val.PodUnits, converter.unitOperations)
 		} else if converter.Kind == "display" && val.DisplayUnits != "" {
-			converter.operations[val.ID] = models.NewOperations(getCustomOrGlobalOperations(val.DisplayUnits, converter.unitOperations))
+			ops = getCustomOrGlobalOperations(val.DisplayUnits, converter.unitOperations)
+		} else {
+			converter.trace.Trace().Str("id", val.ID).Msg("no unit conversion")
+			continue
 		}
+		converter.trace.Trace().Str("operations", ops).Str("id", val.ID).Msg("add units")
+		converter.operations[val.ID] = models.NewOperations(ops)
 	}
 }
 
@@ -59,6 +72,7 @@ func (converter *UnitConverter) Convert(values map[string]any) map[string]any {
 			convertedValues[name] = value
 		}
 	}
+	converter.trace.Trace().Msg("convert")
 	return convertedValues
 }
 
@@ -71,5 +85,6 @@ func (converter *UnitConverter) Revert(values map[string]any) map[string]any {
 			convertedValues[name] = value
 		}
 	}
+	converter.trace.Trace().Msg("convert")
 	return convertedValues
 }
