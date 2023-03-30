43e14f90079c3d63cf031d29c1582c1142da4480
jmaralo <martinezalonsojn@gmail.com>
2023-03-30 16:48:57 +0200

    DEV-122 add tracing to vehicle

--- src/vehicle/internals/update_factory.go
+++ src/vehicle/internals/update_factory.go
@@ -5,22 +5,28 @@ import (
 	"time"
 
 	"github.com/HyperloopUPV-H8/Backend-H8/vehicle/models"
+	"github.com/rs/zerolog"
+	trace "github.com/rs/zerolog/log"
 )
 
 type UpdateFactory struct {
 	count     map[uint16]uint64
 	timestamp map[uint16]uint64
+	trace     zerolog.Logger
 }
 
 func NewFactory() *UpdateFactory {
+	trace.Info().Msg("new update factory")
 	return &UpdateFactory{
 		count:     make(map[uint16]uint64),
 		timestamp: make(map[uint16]uint64),
+		trace:     trace.With().Str("component", "updateFactory").Logger(),
 	}
 }
 
 func (factory UpdateFactory) NewUpdate(id uint16, hexValue []byte, fields map[string]any) models.Update {
 	count, cycleTime := factory.getNext(id)
+	factory.trace.Trace().Uint16("id", id).Uint64("count", count).Uint64("cycleTime", cycleTime).Msg("new update")
 	return models.Update{
 		ID:        id,
 		HexValue:  fmt.Sprintf("%x", hexValue),
