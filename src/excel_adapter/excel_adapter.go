25b3dfff8116964b9b925b4fdcee982bec310073
jmaralo <martinezalonsojn@gmail.com>
2023-03-30 16:46:46 +0200

    DEV-122 add tracing to excel adapter
    Add tracing and remove some deprecated code

--- src/excel_adapter/excel_adapter.go
+++ src/excel_adapter/excel_adapter.go
@@ -1,13 +1,12 @@
 package excel_adapter
 
 import (
-	"fmt"
-	"log"
 	"path/filepath"
 
 	"github.com/HyperloopUPV-H8/Backend-H8/excel_adapter/internals"
 	internalModels "github.com/HyperloopUPV-H8/Backend-H8/excel_adapter/internals/models"
 	"github.com/HyperloopUPV-H8/Backend-H8/excel_adapter/models"
+	trace "github.com/rs/zerolog/log"
 	"github.com/xuri/excelize/v2"
 )
 
@@ -18,42 +17,57 @@ const PORTS_TABLE_NAME = "ports"
 const IDS_TABLE_NAME = "ids"
 
 func FetchDocument(id string, path string, name string) internalModels.Document {
+	trace.Info().Str("id", id).Str("path", path).Str("name", name).Msg("fetch document")
+
 	errDownloading := internals.DownloadFile(id, path, name)
 	if errDownloading != nil {
-		log.Println("USING LOCAL FILE")
+		trace.Error().Err(errDownloading).Msg("")
+		trace.Warn().Str("id", id).Str("path", path).Str("name", name).Msg("using local document")
 	}
+
 	file, err := excelize.OpenFile(filepath.Join(path, name))
 	if err != nil {
-		log.Fatalf("excel adapter: FetchDocument: %s\n", err)
+		trace.Fatal().Err(err).Msg("")
 	}
+
 	return internals.GetDocument(file)
 }
 
 func getBoards(document internalModels.Document) map[string]models.Board {
+	trace.Trace().Msg("get boards")
 	boards := make(map[string]models.Board, len(document.BoardSheets)-1)
 	for name, sheet := range document.BoardSheets {
+		trace.Trace().Str("board", name).Msg("add board")
 		boards[name] = models.NewBoard(name, getIP(name, document), sheet)
 	}
 	return boards
 }
 
-func getIP(sheet string, document internalModels.Document) string {
+func getIP(board string, document internalModels.Document) string {
 	for _, row := range document.Info.Tables[ADDRESSES_TABLE_NAME].Rows {
-		if row[0] == sheet {
+		if row[0] == board {
+			trace.Trace().Str("board", board).Str("addr", row[1]).Msg("get board ip")
 			return row[1]
 		}
 	}
-	panic(fmt.Sprintf("excel adapter: getIP: Missing board %s IP\n", sheet))
+
+	trace.Fatal().Str("board", board).Msg("missing board ip")
+	return ""
 }
 
 func Update(document internalModels.Document, objects ...models.FromDocument) {
+	trace.Debug().Msg("update from document")
+
+	trace.Trace().Msg("update global info")
 	globalInfo := getGlobalInfo(document)
 	for _, object := range objects {
 		object.AddGlobal(globalInfo)
 	}
 
-	for _, board := range getBoards(document) {
+	for name, board := range getBoards(document) {
+		trace.Trace().Str("board", name).Msg("update board")
 		for _, packet := range board.GetPackets() {
+			trace.Trace().Str("packet", packet.Description.ID).Msg("update packet")
 			for _, object := range objects {
 				object.AddPacket(board.Name, packet)
 			}
@@ -62,6 +76,7 @@ func Update(document internalModels.Document, objects ...models.FromDocument) {
 }
 
 func getGlobalInfo(document internalModels.Document) models.GlobalInfo {
+	trace.Trace().Msg("get global info")
 	return models.GlobalInfo{
 		BoardToIP:        getInfoTableToMap(ADDRESSES_TABLE_NAME, document),
 		UnitToOperations: getInfoTableToMap(UNITS_TABLE_NAME, document),
@@ -70,39 +85,17 @@ func getGlobalInfo(document internalModels.Document) models.GlobalInfo {
 	}
 }
 
-func getExpandedSection(section models.ControlSection, board models.Board) models.ControlSection {
-	expandedSection := make(map[string]interface{})
-	for guiName, valueNameInterface := range section {
-		valueName := valueNameInterface.(string)
-		packetName := board.FindContainingPacket(valueName)
-		allIds := internals.GetAllIds(board.Descriptions[packetName].ID)
-
-		if len(allIds) == 1 {
-			expandedSection[guiName] = valueName
-		} else {
-			expandedSection[guiName] = getNamesWithSufix(valueName, len(allIds))
-		}
-
-	}
-	return expandedSection
-}
-
-func getNamesWithSufix(name string, length int) []string {
-	namesWithSufix := make([]string, length)
-	for i := 0; i < length; i++ {
-		namesWithSufix[i] = fmt.Sprintf("%s_%d", name, i)
-	}
-	return namesWithSufix
-}
-
 func getInfoTableToMap(tableName string, document internalModels.Document) map[string]string {
 	mapping := make(map[string]string)
 	table, found := document.Info.Tables[tableName]
 	if !found {
-		log.Fatalf("excel adapter: getInfoTableToMap: table %s not found\n", tableName)
+		trace.Fatal().Str("table", tableName).Msg("table not found")
+		return nil
 	}
+
 	for _, row := range table.Rows {
 		mapping[row[0]] = row[1]
 	}
+	trace.Trace().Str("table", tableName).Msg("get info table")
 	return mapping
 }
--- src/excel_adapter/internals/excel_retriever.go
+++ src/excel_adapter/internals/excel_retriever.go
@@ -3,38 +3,44 @@ package internals
 import (
 	"context"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 
+	trace "github.com/rs/zerolog/log"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
 )
 
-const sheetsMimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+const SHEETS_MIME_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
 func DownloadFile(id string, path string, name string) error {
+	trace.Trace().Str("id", id).Str("path", path).Str("name", name).Msg("download file")
 	client, errClient := getClient()
 	if errClient != nil {
+		trace.Error().Str("id", id).Str("path", path).Str("name", name).Err(errClient).Msg("")
 		return errClient
 	}
 
-	file, errFile := getFile(client, id, sheetsMimeType)
+	file, errFile := getFile(client, id, SHEETS_MIME_TYPE)
 	if errFile != nil {
+		trace.Error().Str("id", id).Str("path", path).Str("name", name).Err(errFile).Msg("")
 		return errFile
 	}
 
 	errSaving := saveFile(file, path, name)
+	if errSaving != nil {
+		trace.Error().Str("id", id).Str("path", path).Str("name", name).Err(errSaving).Msg("")
+	}
 
 	return errSaving
 }
 
 func getClient() (*drive.Service, error) {
+	trace.Trace().Msg("get client")
 	ctx := context.Background()
 
 	client, err := drive.NewService(ctx, option.WithCredentialsFile(os.Getenv("CREDENTIALS")))
 	if err != nil {
-		log.Printf("excel retriever: the client could not be obtained")
 		return nil, err
 	}
 
@@ -42,16 +48,15 @@ func getClient() (*drive.Service, error) {
 }
 
 func getFile(client *drive.Service, id string, mimeType string) ([]byte, error) {
+	trace.Trace().Str("id", id).Msg("get file")
 	resp, err := client.Files.Export(id, mimeType).Download()
 	if err != nil {
-		log.Printf("excel retriever: getFile: could not download the file: %s\n", err)
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("excel retriever: getFile: could not read the http response body")
 		return nil, err
 	}
 
@@ -59,14 +64,15 @@ func getFile(client *drive.Service, id string, mimeType string) ([]byte, error) 
 }
 
 func saveFile(content []byte, path string, name string) error {
+	trace.Trace().Str("path", path).Str("name", name).Msg("save file")
+
 	err := os.Mkdir(path, os.ModeDir)
 	if !os.IsExist(err) {
-		log.Printf("excel retriever: saveFile: could not create the directory")
 		return err
 	}
+
 	err = os.WriteFile(filepath.Join(path, name), content, 0644) // rw-r--r--
 	if err != nil {
-		log.Printf("excel retriever: saveFile: could not write the file in the directory")
 		return err
 	}
 	return nil
