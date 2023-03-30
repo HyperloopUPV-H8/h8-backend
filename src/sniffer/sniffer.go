882ba38be8a5c10b07b4f3ddb52a2b5ceaf70c2e
jmaralo <martinezalonsojn@gmail.com>
2023-03-30 16:48:26 +0200

    DEV-122 add tracing to sniffer

--- src/sniffer/sniffer.go
+++ src/sniffer/sniffer.go
@@ -4,6 +4,8 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"github.com/rs/zerolog"
+	trace "github.com/rs/zerolog/log"
 )
 
 const SNAPLEN = 1500
@@ -11,20 +13,27 @@ const SNAPLEN = 1500
 type Sniffer struct {
 	source *pcap.Handle
 	filter string
+	trace  zerolog.Logger
 }
 
 func New(dev string, filter string) (*Sniffer, error) {
+	trace.Info().Msg("new sniffer")
+
 	source, err := obtainSource(dev, filter)
 	if err != nil {
+		trace.Error().Err(err).Msg("")
 		return nil, err
 	}
+
 	return &Sniffer{
 		source: source,
 		filter: filter,
+		trace:  trace.With().Str("component", "sniffer").Str("dev", dev).Logger(),
 	}, nil
 }
 
 func obtainSource(dev string, filter string) (*pcap.Handle, error) {
+	trace.Debug().Str("dev", dev).Str("filter", filter).Msg("obtain source")
 	source, err := pcap.OpenLive(dev, SNAPLEN, true, pcap.BlockForever)
 	if err != nil {
 		return nil, err
@@ -38,6 +47,7 @@ func obtainSource(dev string, filter string) (*pcap.Handle, error) {
 }
 
 func (sniffer *Sniffer) Listen(output chan<- []byte) <-chan error {
+	sniffer.trace.Info().Msg("start listening")
 	errorChan := make(chan error)
 
 	go sniffer.read(output, errorChan)
@@ -49,10 +59,13 @@ func (sniffer *Sniffer) read(output chan<- []byte, errorChan chan<- error) {
 	for {
 		raw, _, err := sniffer.source.ReadPacketData()
 		if err != nil {
+			sniffer.trace.Error().Err(err).Msg("")
 			errorChan <- err
 			close(errorChan)
 			return
 		}
+
+		sniffer.trace.Trace().Msg("read")
 
 		output <- gopacket.NewPacket(raw, layers.LayerTypeEthernet, gopacket.DecodeOptions{
 			Lazy:   true,
