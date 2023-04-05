4d7b5f4cb9d13754da04908dfc05a4c652bfbcfb
Sergio <smorsua@teleco.upv.es>
2023-04-05 18:23:48 +0200

    Removed cmd folder

---
+++ src/config.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/HyperloopUPV-H8/Backend-H8/connection_transfer"
+	"github.com/HyperloopUPV-H8/Backend-H8/data_transfer"
+	"github.com/HyperloopUPV-H8/Backend-H8/excel_adapter"
+	"github.com/HyperloopUPV-H8/Backend-H8/log_handle"
+	"github.com/HyperloopUPV-H8/Backend-H8/message_transfer"
+	"github.com/HyperloopUPV-H8/Backend-H8/server"
+	"github.com/HyperloopUPV-H8/Backend-H8/vehicle"
+)
+
+type Config struct {
+	Excel        excel_adapter.ExcelAdapterConfig
+	Connections  connection_transfer.ConnectionTransferConfig
+	Logger       log_handle.LoggerConfig
+	Vehicle      vehicle.BuilderConfig
+	DataTransfer data_transfer.DataTransferConfig `toml:"data_transfer"`
+	Orders       struct {
+		SendTopic string
+	}
+	Messages message_transfer.MessageTransferConfig
+	Server   server.ServerConfig
+}
