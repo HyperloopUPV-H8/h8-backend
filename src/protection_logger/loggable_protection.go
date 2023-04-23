cbc87413395f0a13ec99655e568853b4a5765617
Sergio <smorsua@teleco.upv.es>
2023-04-23 10:04:33 +0200

    Refactored loggable_protection

--- src/protection_logger/loggable_protection.go
+++ src/protection_logger/loggable_protection.go
@@ -16,58 +16,26 @@ func (lp LoggableProtection) Log() []string {
 	date := fmt.Sprintf("%d/%d/%d", lp.Timestamp.Day, lp.Timestamp.Month, lp.Timestamp.Year)
 	time := fmt.Sprintf("%d:%d:%d", lp.Timestamp.Hours, lp.Timestamp.Minutes, lp.Timestamp.Seconds)
 	datetime := fmt.Sprintf("%s %s", date, time)
+	violationData := getViolationString(lp.Violation)
+	return []string{datetime, lp.Kind, lp.Board, lp.Value, violationData}
+}
 
-	switch violation := lp.Violation.(type) {
+func getViolationString(violation vehicle_models.Violation) string {
+	var violationData string
+	switch violation := violation.(type) {
 	case vehicle_models.OutOfBoundsViolation:
-		return []string{
-			datetime,
-			lp.Kind,
-			lp.Board,
-			lp.Value,
-			violation.Kind,
-			fmt.Sprintf("GOT %f", violation.Got),
-			fmt.Sprintf("WANT %v", violation.Want),
-		}
+		violationData = fmt.Sprintf("%s Got: %f Want: %f", violation.Kind, violation.Got, violation.Want)
 	case vehicle_models.LowerBoundViolation:
-		return []string{
-			datetime,
-			lp.Kind,
-			lp.Board,
-			lp.Value,
-			violation.Kind,
-			fmt.Sprintf("GOT %f", violation.Got),
-			fmt.Sprintf("WANT %f", violation.Want),
-		}
+		violationData = fmt.Sprintf("%s Got: %f Want: > %f", violation.Kind, violation.Got, violation.Want)
 	case vehicle_models.UpperBoundViolation:
-		return []string{
-			datetime,
-			lp.Kind,
-			lp.Board,
-			lp.Value,
-			violation.Kind,
-			fmt.Sprintf("GOT %f", violation.Got),
-			fmt.Sprintf("WANT < %f", violation.Want),
-		}
+		violationData = fmt.Sprintf("%s Got: %f Want: < %f", violation.Kind, violation.Got, violation.Want)
 	case vehicle_models.EqualsViolation:
-		return []string{
-			datetime,
-			lp.Kind,
-			lp.Board,
-			lp.Value,
-			violation.Kind,
-			fmt.Sprintf("GOT %f", violation.Got),
-		}
+		violationData = fmt.Sprintf("%s %f is not allowed", violation.Kind, violation.Got)
 	case vehicle_models.NotEqualsViolation:
-		return []string{
-			datetime,
-			lp.Kind,
-			lp.Board,
-			lp.Value,
-			violation.Kind,
-			fmt.Sprintf("GOT %f", violation.Got),
-		}
+		violationData = fmt.Sprintf("%s %f should be %f", violation.Kind, violation.Got, violation.Want)
 	default:
-		return []string{"MESSAGE NOT RECOGNIZED", lp.Board, lp.Kind, lp.Value}
+		violationData = fmt.Sprintf("%s UNRECOGNIZED VIOLATION", violation.Type())
 	}
 
+	return violationData
 }
