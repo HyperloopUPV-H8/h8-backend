5d55289359f06158ff624dd74fab12148e251514
smorsua <smorsua@teleco.upv.es>
2023-06-14 11:02:56 +0200

    Fixed race condition

--- src/common/observable/replay_observable.go
+++ src/common/observable/replay_observable.go
@@ -1,24 +1,40 @@
 package observable
 
+import "sync"
+
 type ReplayObservable[T any] struct {
-	last      T
-	observers map[string]Observer[T]
+	last   T
+	lastMx *sync.Mutex
+
+	observers  map[string]Observer[T]
+	observerMx *sync.Mutex
 }
 
 func NewReplayObservable[T any](initialValue T) ReplayObservable[T] {
 	return ReplayObservable[T]{
-		last:      initialValue,
-		observers: make(map[string]Observer[T], 0),
+		last:   initialValue,
+		lastMx: &sync.Mutex{},
+
+		observers:  make(map[string]Observer[T], 0),
+		observerMx: &sync.Mutex{},
 	}
 }
 
 func (o *ReplayObservable[T]) Next(value T) {
+	o.lastMx.Lock()
+	defer o.lastMx.Unlock()
 	o.last = value
+
 	o.broadcast(value)
 }
 
 func (o *ReplayObservable[T]) Subscribe(observer Observer[T]) {
+	o.observerMx.Lock()
+	defer o.observerMx.Unlock()
 	o.observers[observer.Id()] = observer
+
+	o.lastMx.Lock()
+	defer o.lastMx.Unlock()
 	observer.Next(o.last)
 }
 
@@ -27,6 +43,8 @@ func (o *ReplayObservable[T]) Unsubscribe(id string) {
 }
 
 func (o *ReplayObservable[T]) broadcast(value T) {
+	o.observerMx.Lock()
+	defer o.observerMx.Unlock()
 	for _, observer := range o.observers {
 		observer.Next(value)
 	}
