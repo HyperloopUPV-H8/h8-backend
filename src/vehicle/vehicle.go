a19c96253051dcf58e69e5d4d985c069fbcd9806
jmaralo <martinezalonsojn@gmail.com>
2023-06-14 04:40:26 +0200

    DEV-193 propagate fault

--- src/vehicle/vehicle.go
+++ src/vehicle/vehicle.go
@@ -43,6 +43,12 @@ type Vehicle struct {
 	trace zerolog.Logger
 }
 
+func (vehicle *Vehicle) propagateFault(source string, payload []byte) {
+	for _, pipe := range vehicle.pipes {
+		pipe.SendFault(source, payload)
+	}
+}
+
 func (vehicle *Vehicle) Listen(updateChan chan<- models.PacketUpdate, transmittedOrderChan chan<- models.PacketUpdate, messageChan chan<- any, blcuAckChan chan<- struct{}, stateOrdersChan chan<- message_parser.StateOrdersAdapter) {
 	vehicle.trace.Debug().Msg("vehicle listening")
 	for packet := range vehicle.dataChan {
@@ -51,6 +57,10 @@ func (vehicle *Vehicle) Listen(updateChan chan<- models.PacketUpdate, transmitte
 
 		if packet.Metadata.ID == 0 {
 			continue
+		}
+
+		if packet.Metadata.ID == 1 {
+			vehicle.propagateFault(packet.Metadata.From, packet.Payload)
 		}
 
 		//TODO: add order decoding
