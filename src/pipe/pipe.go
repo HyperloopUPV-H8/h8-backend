bd6996fd68c23705f5edca8891f352f5eadb3c3d
jmaralo <martinezalonsojn@gmail.com>
2023-07-20 05:56:01 +0200

    DEV-200 Send keepalives

--- src/pipe/pipe.go
+++ src/pipe/pipe.go
@@ -39,10 +39,6 @@ func (pipe *Pipe) connect() {
 		LocalAddr: pipe.laddr,
 	}
 
-	if pipe.keepaliveInterval != nil {
-		dialer.KeepAlive = *pipe.keepaliveInterval
-	}
-
 	for pipe.isClosed {
 		pipe.trace.Trace().Msg("dial")
 
@@ -61,12 +57,26 @@ func (pipe *Pipe) connect() {
 	go pipe.listen()
 }
 
+func (pipe *Pipe) keepalive(interval *time.Duration) {
+	ticker := time.NewTicker(*interval)
+	for range ticker.C {
+		_, err := pipe.Write([]byte{0x45, 0x00})
+		if err != nil {
+			return
+		}
+	}
+
+}
+
 func (pipe *Pipe) open(conn *net.TCPConn) {
 	pipe.trace.Debug().Msg("open")
 	pipe.conn = conn
 	pipe.isClosed = false
 	pipe.conn.SetNoDelay(true)
 	pipe.onConnectionChange(!pipe.isClosed)
+	if pipe.keepaliveInterval != nil {
+		go pipe.keepalive(pipe.keepaliveInterval)
+	}
 }
 
 func (pipe *Pipe) listen() {
